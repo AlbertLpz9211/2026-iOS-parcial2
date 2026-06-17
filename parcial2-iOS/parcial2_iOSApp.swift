c5c1a5dbc104c50a6e57a51cbccdc4849cb36b30
Alberto Lopez <alberto.lopez_g@outlook.com>
2026-06-17 13:28:05 -0700

    Initial Commit

---
+++ parcial2-iOS/parcial2_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  parcial2_iOSApp.swift
+//  parcial2-iOS
+//
+//  Created by Alberto Lopez on 17/06/26.
+//
+
+import SwiftUI
+
+@main
+struct parcial2_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
